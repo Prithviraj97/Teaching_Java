4f3863937f6ee0dec707c1f3d358c8252d83498a
Prithviraj97 <prithvirajsingh358@gmail.com>
2025-02-03 20:19:51 -0600

    loop demo I

---
+++ src/CS281_S25/MultiplicationTable.java
@@ -0,0 +1,17 @@
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 10;
+		
+		//generate multiplication matrix
+		for(int i = 1; i <= num; i++) {
+			for(int j = 1; j <=num; j++) {
+				System.out.printf("%4d", i*j);
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ src/CS281_S25/SumofDigits.java
@@ -0,0 +1,18 @@
+
+public class SumofDigits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 384;
+		int OriginalNum = num;
+		int sum = 0;
+		
+		//use a while loop to calculate the sum of digits
+		while(num >0) {
+			int digit = num % 10; //get the last digit (remainder of division by 10)
+			sum += digit;
+			num /= 10;
+		}
+		System.out.println("The sum of digits of " + OriginalNum + " is: " + sum);
+	}
+}
