ca63608b07fd6260f6f38e4d676910163328d232
Prithviraj97 <prithvirajsingh358@gmail.com>
2025-02-15 22:36:11 -0600

    new codes

---
+++ src/CS281_S25/ArraytoArrayList.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArraytoArrayList {
+
+	public static void main(String[] args) {
+		// conversion between arrays and arraylist.
+		String[] array = {"Python", "Java", "C", "Prolog"};
+		//create an array list from array
+		ArrayList<String> langs = new ArrayList<>(Arrays.asList(array));
+		
+		//create an array from array list
+		String[] prog_lang = new String[langs.size()];
+		langs.toArray(prog_lang);
+//		System.out.println(prog_lang);
+		}
+
+	}
+
---
+++ src/CS281_S25/Numbers.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		// create an array list of integers
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(11);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		list.add(47);
+		list.add(56);
+		list.add(63);
+		
+		if(list.isEmpty()) {
+			return;
+		} else {
+			ArrayList<Integer> evenVal = new ArrayList<>();
+			for(int num: list) {
+				if(num %2 == 0) {
+					evenVal.add(num);
+				}
+			}
+			System.out.println(evenVal);
+		}
+		
+//		if(!list.isEmpty()) {
+//			
+//		}
+	}
+
+}
