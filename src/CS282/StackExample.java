93952e1578203061623117f45bf39bbe1105db72
Prithvi Raj Singh <prithvirajsingh358@gmail.com>
2025-04-16 15:08:23 -0500

    minor update

--- src/CS282/StackExample.java
+++ src/CS282/StackExample.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Stack<T> {
-    private List<T> items;
+    final private List<T> items;
 
     public Stack() {
         items = new ArrayList<>();
