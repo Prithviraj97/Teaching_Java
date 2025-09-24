6ebe980c3985a184871443c675b9c3a79fbd6895
Prithvi Raj Singh <prithvirajsingh358@gmail.com>
2025-09-24 16:39:32 -0500

    initialized for sum of arraylist

---
+++ src/CS281/SumArraylist.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+public class SumArraylist {
+    public static void main(String[] args) {
+      ArrayList<Integer> numbers = new java.util.ArrayList<>();
+        
+    }
+}
